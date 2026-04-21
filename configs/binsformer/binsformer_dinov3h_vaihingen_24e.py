02ab2c74442c6a206e3dfb4d14cca592a57221ad
lanjie <1355008426@qq.com>
2026-04-21 11:03:49 +0800

    Revert "Reduce DINOv3 huge training memory"
    This reverts commit ef76b2e9d88771f01211fbe5999786bdc6d1227e.

--- configs/binsformer/binsformer_dinov3h_vaihingen_24e.py
+++ configs/binsformer/binsformer_dinov3h_vaihingen_24e.py
@@ -1,5 +1,3 @@
-import os
-
 _base_ = ['./binsformer_dinov3b_vaihingen_24e.py']
 
 
@@ -12,7 +10,6 @@ model = dict(
         # DINOv3 huge has 32 transformer blocks; sample four evenly spaced stages.
         out_indices=(7, 15, 23, 31),
         output_cls_token=True,
-        with_cp=True,
         pretrained='pretrained/dinov3_huge.pth'),
     neck=dict(
         type='DINOv3AdaBinsNeck',
@@ -23,9 +20,3 @@ model = dict(
     decode_head=dict(
         type='BinsFormerDecodeHead',
         in_channels=[160, 320, 640, 1280]))
-
-# DINOv3 huge is substantially heavier than base; default to a safer batch size
-# while still allowing server-side overrides via the existing env var.
-data = dict(samples_per_gpu=int(os.getenv('VAIHINGEN_SAMPLES_PER_GPU', '1')))
-
-del os
