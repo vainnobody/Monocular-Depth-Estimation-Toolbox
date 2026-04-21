64a2a5690dcc0c151d666e7ae48d4f2aec9aab57
lanjie <1355008426@qq.com>
2026-04-21 09:54:29 +0800

    Add BinsFormer DINOv3 huge Vaihingen config

---
+++ configs/binsformer/binsformer_dinov3h_vaihingen_24e.py
@@ -0,0 +1,22 @@
+_base_ = ['./binsformer_dinov3b_vaihingen_24e.py']
+
+
+model = dict(
+    backbone=dict(
+        _delete_=True,
+        type='DINOv3Backbone',
+        model_name='huge',
+        img_size=512,
+        # DINOv3 huge has 32 transformer blocks; sample four evenly spaced stages.
+        out_indices=(7, 15, 23, 31),
+        output_cls_token=True,
+        pretrained='pretrained/dinov3_huge.pth'),
+    neck=dict(
+        type='DINOv3AdaBinsNeck',
+        in_channels=1280,
+        out_channels=[160, 320, 640, 1280],
+        readout_type='project',
+        patch_size=16),
+    decode_head=dict(
+        type='BinsFormerDecodeHead',
+        in_channels=[160, 320, 640, 1280]))
